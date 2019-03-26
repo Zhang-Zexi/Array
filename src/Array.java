febe7c7d51637c9a7bc1330120000ad420a697b9
zhangzx <zhangzx@allinfinance.com>
2019-03-26 18:16:48 +0800

    动态数组

--- src/Array.java
+++ src/Array.java
@@ -125,7 +125,7 @@ public class Array<E> {
         size -- ;
         data[size] = null; //loitering objects != memory leak
 
-        if(size == data.length/2) {
+        if(size == data.length/4) {
             resize(data.length/2);
         }
         return ret;
